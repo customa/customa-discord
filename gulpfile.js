8aae856dd06bf42c34d71ae396adb0614f7935bf
GhostlyDilemma <ghostly.dilemma@gmail.com>
2020-04-05 16:46:34 +0200

    Unbreak Pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -13,21 +13,15 @@ var modules = [
 ];
 var paths = {
   src: modules.map(dir => dir + "/**/*.m.css"),
-  destMin: "dist",
+  dest: "dist",
   destSrc: "src"
 };
-
-function copy() {
-  return gulp
-    .src(paths.src, { base: "." })
-    .pipe(gulp.dest(paths.destSrc))
-}
 
 function minify() {
   return gulp
     .src(paths.src, { base: "." })
     .pipe(cleanCss())
-    .pipe(gulp.dest(paths.destMin));
+    .pipe(gulp.dest(paths.dest));
 }
 
 exports.default = minify;
